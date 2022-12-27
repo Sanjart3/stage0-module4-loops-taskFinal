c6fcd6b32b9502bb66b1e699f00e2d5791325d67
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-27 12:20:24 +0500

    just

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -1,11 +1,21 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
-    public void printHalfPyramid(int cathetusLength) {
 
+    public void printHalfPyramid(int cathetusLength) {
+        for (int i=1;i<=cathetusLength;i++){
+            for (int a=1;a<=cathetusLength-i;a++){
+                System.out.print(" ");
+            }
+            for (int s=1;s<=i;s++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
+
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,11 +1,23 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
 
+    public void printPyramid(int cathetusLength) {
+        String word="";
+        for (int i=1;i<=cathetusLength;i++){
+            for (int k=1;k<=cathetusLength-i;k++){
+                word+=" ";
+            }
+            for (int s=1;s<=2*i-1;s++){
+                word+=Math.abs(i-s)+1;
+            }
+            System.out.println(word);
+            word = "";
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
+
 }
